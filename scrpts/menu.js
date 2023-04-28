69083c195af71655d7601d74e21f6ab93626b7d2
Michelle <michellelaurian@gmail.com>
2023-04-28 14:39:56 -0300

    menu cards

--- scrpts/menu.js
+++ scrpts/menu.js
@@ -1,33 +1,40 @@
-//  seleccionamos los dos elementos que serán clickables
+// //  seleccionamos los dos elementos que serán clickables
 
-const toggleButton = document.getElementById("button-menu")
-const navWrapper = document.getElementById("nav")
-const menudisplay = document.getElementById("container_body")
-const menuFixed = document.getElementById("menu-fixed")
+// const toggleButton = document.getElementById("button-menu")
+// const navWrapper = document.getElementById("nav")
+// const menudisplay = document.getElementById("container_body")
+// const menuFixed = document.getElementById("menu-fixed")
 
-/* 
-  cada ves que se haga click en el botón 
-  agrega y quita las clases necesarias 
-  para que el menú se muestre.
-*/
-toggleButton.addEventListener("click", () => {
-  toggleButton.classList.toggle("close")
-  navWrapper.classList.toggle("show")
-  menudisplay.classList.toggle("ocultar")
+// /*
+//   cada ves que se haga click en el botón
+//   agrega y quita las clases necesarias
+//   para que el menú se muestre.
+// */
+// toggleButton.addEventListener("click", () => {
+//   toggleButton.classList.toggle("close")
+//   navWrapper.classList.toggle("show")
+//   menudisplay.classList.toggle("ocultar")
+// })
+
+// /*
+//   Cuándo se haga click fuera del contenedor de enlaces
+//   el menú debe esconderse.
+// */
+
+// navWrapper.addEventListener("click", (e) => {
+//   if (e.target.id === "nav") {
+//     navWrapper.classList.remove("show")
+//     toggleButton.classList.remove("close")
+//   }
+// })
+
+// // document.body.onscroll = () =>{
+// //   menuFixed.classList.add("menuFixes")
+// // }
+
+const toggleMenuElement = document.getElementById("toggle-menu")
+const mainMenuElement = document.getElementById("main-menu")
+
+toggleMenuElement.addEventListener("click", () => {
+  mainMenuElement.classList.toggle("main-menu--show")
 })
-
-/* 
-  Cuándo se haga click fuera del contenedor de enlaces 
-  el menú debe esconderse.
-*/
-
-navWrapper.addEventListener("click", (e) => {
-  if (e.target.id === "nav") {
-    navWrapper.classList.remove("show")
-    toggleButton.classList.remove("close")
-  }
-})
-
-// document.body.onscroll = () =>{
-//   menuFixed.classList.add("menuFixes")
-// }
